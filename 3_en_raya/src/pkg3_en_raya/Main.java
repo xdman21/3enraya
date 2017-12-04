a249243029b94bf04fc2b52e4539b65549fd3bc8
JairoValverde <32849071+JairoValverde@users.noreply.github.com>
2017-12-04 20:15:05 +0100

    modificar main

--- 3_en_raya/src/pkg3_en_raya/Main.java
+++ 3_en_raya/src/pkg3_en_raya/Main.java
@@ -12,30 +12,33 @@ import java.io.InputStreamReader;
  *
  * @author xavi
  */
-public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
+public class Main 
+{
+    public static void main(String[] args) throws IOException 
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int i = 1;
-        String jugador1 = "X";
-        String jugador2 = "J";
         System.out.println("Escribe nombre Jugador1");
         String nombrejugador1 = br.readLine();
+        
         System.out.println("Escribe Nombre Jugador 2");
         String nombrejugador2 = br.readLine();
-        tablero t = new tablero(jugador1,jugador2,nombrejugador1,nombrejugador2);
-        System.out.println();
+        
+        tablero t = new tablero();
+
         t.crearTablero();
         t.imprimirtablero();
         
-        while(i != 9) 
+        while(i <= 4) 
         {
             t.moverjugador1();
             t.moverjugador2();
+            
+            i++;
         }
+        
+        t.moverjugador1();
     }
-    
 }
